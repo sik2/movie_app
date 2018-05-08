0533940edde6c57b48a89f880118b751a34a0180
sik2 <sksik622@naver.com>
2018-05-09 07:39:36 +0900

    movie poster1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import movie from './movie';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+          <movie/>
       </div>
     );
   }
