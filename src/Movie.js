3f1e2454c052e0cf1b366507c186c0210a208a41
sik2 <sksik622@naver.com>
2018-05-14 03:09:43 +0900

    ajax finish

--- src/Movie.js
+++ src/Movie.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 import './Movie.css';
+import PropTypes from "prop-types";
 
 class Movie extends Component {
+
+    static propTypes = {
+        title: PropTypes.string.isRequired,
+        poster: PropTypes.string.isRequired
+    }
+
     render() {
 
         return(
@@ -14,6 +21,9 @@ class Movie extends Component {
     }
 }
 class MoviePoster extends Component {
+    static propTypes = {
+        poster: PropTypes.string.isRequired
+    }
     render() {
 
         return(
